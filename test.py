9f3d80f3886d81eae36c753f92ef607a44287fc5
deramer <arseniy.nestyuk@gmail.com>
2019-09-28 15:46:06 +0300

    Fix code, add max-line-length

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-# some bad python code
-from math import *
-a=56+12
-def f(a,b,c):
-    return sin(a*b*c)
-f(1,2,3)
+from math import sin
+
+
+def f(a, b, c):
+    return sin(a * b * c)
+
+
+a = 56 + 12
+f(1, 2, 3)
+very_long_line = 'very_long_string' + 'another_very_long_string' + 'yet_another_very_long_string'
